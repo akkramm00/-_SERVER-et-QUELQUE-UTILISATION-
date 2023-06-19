4630271fe7920a5903eff712591df0cd89450d9a
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-19 07:28:54 +0000

    add all

--- index.php
+++ index.php
@@ -28,6 +28,25 @@ $server_soft = $_SERVER['SERVER_SOFTWARE'];
 echo " Le nom du serveur est :" .$server_soft ; // récuoer le nom du serveur;
 echo "<br>" ;
 
+//==================================================================
+
+//             LE SUPERGLOBAL  $_SESSION 
+
+session_start(); // initialisation de la session .
+
+$_SESSION['username'] = 'JohnDoe' ; // Stocker les informations dans la session.
+
+// Afficher quelques informations sur le serveur 
+echo 'Nom du serveur :' . $_SERVER['SERVER_NAME'].'<br>';
+echo 'Adresse IP du client : '.$_SERVER['REMOTE_ADDR'] .'<br>';
+echo 'Méthode la requete HTTP : ' .$_SERVER['REQUEST_METHOD'] . '<br>' ;
+
+// Affiche une variable d'environnement (peut ne pas etre définie sur tous les serveurs ).
+if(isset($_ENV['PATH'])){
+  echo 'chemin d\'accès aux exécutables :' . $_ENV['PATH'] .  '<br>' ;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    
+    }
 
 
 
@@ -35,7 +54,8 @@ echo "<br>" ;
 
 
 
-     ?> 
+
+?> 
 
 
 
